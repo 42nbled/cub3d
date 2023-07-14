1b6c65c4573482efb44e420f625fde87f75f46ef
Clement DE SEDE <cde-sede@paul-f4Br6s8.clusters.42paris.fr>
2023-07-14 14:56:51 +0200

    raycasting

--- srcs/raycasting.c
+++ srcs/raycasting.c
@@ -6,28 +6,29 @@
 /*   By: nbled <nbled@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/06 21:55:50 by nbled             #+#    #+#             */
-/*   Updated: 2023/07/06 22:00:40 by nbled            ###   ########.fr       */
+/*   Updated: 2023/07/14 14:56:30 by cde-sede         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <cub3d.h>
 
-t_vec is_lower_vec(t_data *data, t_vec *end_x, t_vec *end_y)
+t_vec	is_lower_vec(t_data *data, t_vec *end_x, t_vec *end_y)
 {
-    double dist_x;
-    double dist_y;
+	double	dist_x;
+	double	dist_y;
 
-    dist_x = sqrt(pow(fabs(end_x->x - data->player_x * 50), 2) + pow(fabs(end_x->y - data->player_y * 50), 2));
-    dist_y = sqrt(pow(fabs(end_y->x - data->player_x * 50), 2) + pow(fabs(end_y->y - data->player_y * 50), 2));
-    if (dist_x < dist_y)
-        return *end_x;
-    return *end_y;
+	dist_x = sqrt(pow(fabs(end_x->x - data->player_x * 50), 2)
+			+ pow(fabs(end_x->y - data->player_y * 50), 2));
+	dist_y = sqrt(pow(fabs(end_y->x - data->player_x * 50), 2)
+			+ pow(fabs(end_y->y - data->player_y * 50), 2));
+	if (dist_x < dist_y)
+		return (*end_x);
+	return (*end_y);
 }
 
 t_vec	raycasting(t_data *data)
 {
 	t_vec	dir;
-	//t_vec	pos;
 	t_vec	end_x;
 	t_vec	end_y;
 
@@ -42,24 +43,4 @@ t_vec	raycasting(t_data *data)
 	else
 		end_y = get_dist_y(data, dir, -1, 1);
 	return (is_lower_vec(data, &end_x, &end_y));
-	
-	/*
-	pos.x = data->player_x * 50;
-	pos.y = data->player_y * 50;
-	if (dir.x > 0)
-	{
-		while (pos.x >= 0 && pos.x < end_x.x && pos.y > 0 && pos.y < 400)
-		{
-			pixel_put(data, pos.x, pos.y, 0xFF0000);
-			vec_add(&pos, dir);
-		}
-	}
-	else
-	{
-		while (pos.x > end_x.x && pos.x < 400 && pos.y > 0 && pos.y < 400)
-		{
-			pixel_put(data, pos.x, pos.y, 0xFF0000);
-			vec_add(&pos, dir);
-		}
-	}*/
 }
